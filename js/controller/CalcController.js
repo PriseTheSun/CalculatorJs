b3a44c82fa293820f7dacc0e69401bca9bb2e7ae
Erik Araujo <erik.doctype@gmail.com>
2022-04-02 14:12:43 -0300

    switchCaseOperations

--- js/controller/CalcController.js
+++ js/controller/CalcController.js
@@ -1,62 +1,145 @@
-class CalcController{
-    constructor (){
-        this._locale = 'pt-BR';
-        this._displayCalcEl = document.querySelector("#display");
-        this._dateEl = document.querySelector("#data");
-        this._timeEl = document.querySelector("#hora");
-        this._currentDate; 
-        this.initialize();
-    }
+class CalcController {
+  constructor() {
+    this._operation = [];
+    this._locale = "pt-BR";
+    this._displayCalcEl = document.querySelector("#display");
+    this._dateEl = document.querySelector("#data");
+    this._timeEl = document.querySelector("#hora");
+    this._currentDate;
+    this.initialize();
+    this.initButtonsEvents();
+  }
 
+  initialize() {
+    this.setDisplayDateTime();
 
-    initialize(){   
+    setInterval(() => {
+      this.setDisplayDateTime();
+    }, 1000);
+  }
 
-        this.setDisplayDateTime();
-
-            setInterval(() =>{
-
-                this.setDisplayDateTime();
-
-        }, 1000);
-    }
-
-    setDisplayDateTime(){
-        this.displayDate = this.currentDate.toLocaleDateString(this._locale,{
-            day:"2-digit",
-            month: "long",
-            year: "numeric"
-
-        });
-        this.displayTime = this.currentDate.toLocaleTimeString(this._locale);
-
-    }
+  addEventListenerAll(element, events, fn) {
+    events.split(" ").forEach((event) => {
+      element.addEventListener(event, fn, false);
+    });
+  }
+  //Interações dos botões principais
+  clearAll() {
+ 
+    this._operation = [];
     
-    //get & set Data & Hora;
-    get displayTime(){
-        return this._timeEl.innerHTML;
-    }
-    set displayTime(value){
-        return this._timeEl.innerHTML = value;
-    }
+  }
+  clearEntry() {
 
-    get displayDate(){
-        return this._dateEl.innerHTML;
+    this._operation.pop();
+
+  }
+  setError() {
+
+    this._displayCalc = "Error";
+
+  }
+  addOperation(value){
+
+    this._operation.push(value);
+    
+  }
+  //Ações dos botões princiais
+  exacBtn(value) {
+    switch (value) {
+      case "ac":
+        this.clearAll();
+        break;
+
+      case "ce":
+        this.clearEntry();
+        break;
+
+      case "soma":
+        break;
+
+      case "subitracao":
+        break;
+
+      case "divisao":
+        break;
+
+      case "multiplicacao":
+        break;
+
+      case "porcento":
+        break;
+
+      case "igual":
+        break;
+
+        case '0':
+        case '1':
+        case '2':
+        case '3':
+        case '4':
+        case '5':
+        case '7':
+        case '8':
+        case '9':
+            this.addOperation(parseInt(value));
+
+        break;
+
+      default:
+        this.setError();
+        break;
     }
-    set displayDate(value){
-        return this._dateEl.innerHTML = value;
-    }
-    //get & set do displayCalc;
-    get displayCalc(){
-        return this._displayCalcEl.innerHTML; 
-    }
-    set displayCalc(value){
-        this._displayCalcEl.innerHTML = value;
-    }
-    //get & set do currentDate;
-    get currentDate(){
-        return new Date();
-    }
-    set currentDate(value){
-        this._currentDate = value;
-    }
+  }
+
+  initButtonsEvents() {
+    let buttons = document.querySelectorAll("#buttons > g, #parts > g");
+    buttons.forEach((btn, index) => {
+      this.addEventListenerAll(btn, "click drag mouseover", (e) => {
+        let textBtn = btn.className.baseVal.replace("btn-", "");
+        this.execBtn(textBtn);
+      });
+      this.addEventListenerAll(btn, "mouseover mouseup mousedown", (e) => {
+        btn.style.cursor = "pointer";
+      });
+    });
+  }
+
+  setDisplayDateTime() {
+    this.displayDate = this.currentDate.toLocaleDateString(this._locale, {
+      day: "2-digit",
+      month: "long",
+      year: "numeric",
+    });
+    this.displayTime = this.currentDate.toLocaleTimeString(this._locale);
+  }
+
+  //get & set Data & Hora;
+  get displayTime() {
+    return this._timeEl.innerHTML;
+  }
+  set displayTime(value) {
+    return (this._timeEl.innerHTML = value);
+  }
+
+  get displayDate() {
+    return this._dateEl.innerHTML;
+  }
+  set displayDate(value) {
+    return (this._dateEl.innerHTML = value);
+  }
+  //get & set do displayCalc;
+  get displayCalc() {
+    return this._displayCalcEl.innerHTML;
+  }
+  set displayCalc(value) {
+    this._displayCalcEl.innerHTML = value;
+  }
+  //get & set do currentDate;
+  get currentDate() {
+    return new Date();
+  }
+  set currentDate(value) {
+    this._currentDate = value;
+  }
 }
